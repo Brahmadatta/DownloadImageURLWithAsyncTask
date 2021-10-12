7f5d5c82b347e077b12133eb5fbb601412572fd1
BrahmaDatta Goli <g.brahmadatta@gmail.com>
2021-10-13 00:50:25 +0530

    first commit

---
+++ app/src/main/java/com/example/imageapp/MainActivity.java
@@ -0,0 +1,369 @@
+package com.example.imageapp;
+
+import static android.Manifest.permission.READ_EXTERNAL_STORAGE;
+import static android.Manifest.permission.WRITE_EXTERNAL_STORAGE;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.RequiresApi;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+import androidx.core.app.NotificationCompat;
+import androidx.core.content.ContextCompat;
+
+import android.annotation.SuppressLint;
+import android.annotation.TargetApi;
+import android.app.AlertDialog;
+import android.app.NotificationChannel;
+import android.app.NotificationManager;
+import android.content.BroadcastReceiver;
+import android.content.ContentResolver;
+import android.content.Context;
+import android.content.DialogInterface;
+import android.content.Intent;
+import android.content.IntentFilter;
+import android.content.pm.PackageManager;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.graphics.Color;
+import android.media.AudioAttributes;
+import android.os.AsyncTask;
+import android.os.Build;
+import android.os.Bundle;
+import android.os.Environment;
+import android.provider.Settings;
+import android.util.Log;
+import android.widget.ImageView;
+import android.widget.Toast;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+import java.net.HttpURLConnection;
+import java.net.URL;
+import java.net.URLConnection;
+import java.text.SimpleDateFormat;
+import java.util.ArrayList;
+import java.util.Locale;
+
+public class MainActivity extends AppCompatActivity {
+
+    ImageView imageView;
+    NotificationCompat.Builder mBuilder;
+    NotificationManager notificationManager;
+    int id = 1;
+
+    public static final String NOTIFICATION_CHANNEL_ID = "10001" ;
+    private final static String default_notification_channel_id = "default";
+    String urlsToDownload[] = { "http://www.devoxx.be/wp-content/uploads/2014/10/android.png", "http://www.devoxx.be/wp-content/uploads/2014/10/android.png",
+            "http://www.devoxx.be/wp-content/uploads/2014/10/android.png", "http://www.devoxx.be/wp-content/uploads/2014/10/android.png",
+            "http://www.devoxx.be/wp-content/uploads/2014/10/android.png" };
+    int counter = 0;
+    ArrayList<AsyncTask<String, String, Void>> arr;
+
+    class NotificationReceiver extends BroadcastReceiver {
+        @Override
+        public void onReceive(Context context, Intent intent) {
+            String event = intent.getExtras().getString("NOT_EVENT_KEY");
+            Log.i("NotificationReceiver", "NotificationReceiver onReceive : " + event);
+            if (event.trim().contentEquals("NOT_REMOVED")) {
+                killTasks();
+            }
+        }
+    }
+
+    private void killTasks() {
+        if (null != arr & arr.size() > 0) {
+            for (AsyncTask<String, String, Void> a : arr) {
+                if (a != null) {
+                    Log.i("NotificationReceiver", "Killing download thread");
+                    a.cancel(true);
+                }
+            }
+            notificationManager.cancelAll();
+        }
+    }
+
+    @RequiresApi(api = Build.VERSION_CODES.O)
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        //getBitmapFromURL("https://picsum.photos/200");
+
+        imageView = findViewById(R.id.imageView);
+
+        checkPermission();
+        mBuilder = new NotificationCompat.Builder(MainActivity. this,
+                default_notification_channel_id );
+        NotificationChannel notificationChannel = new NotificationChannel("11" , "channel_name", NotificationManager.IMPORTANCE_HIGH);
+        notificationChannel.enableLights(true);
+        notificationChannel.enableVibration(true);
+        notificationChannel.setVibrationPattern(new long[]{100, 200, 300, 400, 500, 400, 300, 200, 400});
+        notificationManager = (NotificationManager) getSystemService(Context.NOTIFICATION_SERVICE);
+
+        mBuilder.setContentTitle("Downloading images...").setContentText("Download in progress").setSmallIcon(R.mipmap.ic_launcher);
+        // Start a lengthy operation in a background thread
+        notificationManager.createNotificationChannel(notificationChannel);
+
+
+        arr = new ArrayList<AsyncTask<String, String, Void>>();
+        int incr;
+        for (incr = 0; incr < urlsToDownload.length; incr++) {
+            ImageDownloader imageDownloader = new ImageDownloader();
+            imageDownloader.execute(urlsToDownload[incr]);
+            arr.add(imageDownloader);
+        }
+
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        if (!checkPermission()) {
+            new DownloadFiles().execute();
+        } else {
+            if (checkPermission()) {
+                requestPermissionAndContinue();
+            } else {
+                new DownloadFiles().execute();
+            }
+        }
+    }
+
+    private void downloadImagesToSdCard(String downloadUrl, String imageName) {
+        FileOutputStream fos;
+        InputStream inputStream = null;
+
+        try {
+            URL url = new URL(downloadUrl);
+            /* making a directory in sdcard */
+            String sdCard = Environment.getExternalStorageDirectory().toString();
+            File myDir = new File(sdCard, "DemoDownload");
+
+            /* if specified not exist create new */
+            if (!myDir.exists()) {
+                myDir.mkdir();
+                Log.v("", "inside mkdir");
+            }
+
+            /* checks the file and if it already exist delete */
+            String fname = imageName;
+            File file = new File(myDir, fname);
+            Log.d("file===========path", "" + file);
+            if (file.exists())
+                file.delete();
+
+            /* Open a connection */
+            URLConnection ucon = url.openConnection();
+
+            HttpURLConnection httpConn = (HttpURLConnection) ucon;
+            httpConn.setRequestMethod("GET");
+            httpConn.connect();
+            inputStream = httpConn.getInputStream();
+
+            /*
+             * if (httpConn.getResponseCode() == HttpURLConnection.HTTP_OK) {
+             * inputStream = httpConn.getInputStream(); }
+             */
+
+            fos = new FileOutputStream(file);
+            // int totalSize = httpConn.getContentLength();
+            // int downloadedSize = 0;
+            byte[] buffer = new byte[1024];
+            int bufferLength = 0;
+            while ((bufferLength = inputStream.read(buffer)) > 0) {
+                fos.write(buffer, 0, bufferLength);
+                // downloadedSize += bufferLength;
+                // Log.i("Progress:", "downloadedSize:" + downloadedSize +
+                // "totalSize:" + totalSize);
+            }
+            inputStream.close();
+            fos.close();
+            Log.d("test", "Image Saved in sdcard..");
+        } catch (IOException io) {
+            inputStream = null;
+            fos = null;
+            io.printStackTrace();
+        } catch (Exception e) {
+            e.printStackTrace();
+        } finally {
+
+        }
+
+    }
+
+    public class DownloadFiles extends AsyncTask<String, Integer, Bitmap> {
+        @Override
+        protected Bitmap doInBackground(String... sUrl) {
+            Bitmap bm;
+            InputStream in;
+
+            try {
+                URL url = new URL("https://picsum.photos/200");
+                HttpURLConnection connection = (HttpURLConnection) url.openConnection();
+                connection.setDoInput(true);
+                connection.connect();
+                InputStream input = connection.getInputStream();
+                Bitmap myBitmap = BitmapFactory.decodeStream(input);
+                return myBitmap;
+            } catch (IOException e) {
+                e.printStackTrace();
+                return null;
+            }
+        }
+
+        @SuppressLint("WrongThread")
+        @Override
+        protected void onPostExecute(Bitmap bitmap) {
+            super.onPostExecute(bitmap);
+            Log.e("result", bitmap.toString());
+            imageView.setImageBitmap(bitmap);
+            String time = new SimpleDateFormat("yyyyMMdd_HHmmss", Locale.getDefault()).format(System.currentTimeMillis());
+            File path = Environment.getExternalStoragePublicDirectory(
+                    Environment.DIRECTORY_PICTURES);
+            File file = new File(path, "DemoPicture.jpg");
+
+                // Make sure the Pictures directory exists.
+                path.mkdirs();
+                String imagename = time + ".PNG";
+                OutputStream out;
+            try {
+                out = new FileOutputStream(file);
+                bitmap.compress(Bitmap.CompressFormat.PNG, 100, out);
+                out.flush();
+                out.close();
+                Toast.makeText(MainActivity.this, "Image Save To Gallery",
+                        Toast.LENGTH_SHORT).show();
+            } catch (FileNotFoundException e) {
+                e.printStackTrace();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+
+    private class ImageDownloader extends AsyncTask<String, String, Void> {
+
+        @Override
+        protected Void doInBackground(String... param) {
+            downloadImagesToSdCard(param[0], "Image" + counter + ".png");
+            return null;
+        }
+
+        protected void onProgressUpdate(String... values) {
+
+
+        }
+
+        @Override
+        protected void onPreExecute() {
+            Log.i("Async-Example", "onPreExecute Called");
+        }
+
+        @Override
+        protected void onPostExecute(Void result) {
+            Log.i("Async-Example", "onPostExecute Called");
+
+            float len = urlsToDownload.length;
+            // When the loop is finished, updates the notification
+            if (counter >= len - 1) {
+                mBuilder.setContentTitle("Done.");
+                mBuilder.setContentText("Download complete")
+                        // Removes the progress bar
+                        .setProgress(0, 0, false);
+                notificationManager.notify(id, mBuilder.build());
+            } else {
+                int per = (int) (((counter + 1) / len) * 100f);
+                Log.i("Counter", "Counter : " + counter + ", per : " + per);
+                mBuilder.setContentText("Downloaded (" + per + "/100");
+                NotificationManager mNotificationManager = (NotificationManager) getSystemService(Context. NOTIFICATION_SERVICE ) ;
+
+                if (android.os.Build.VERSION. SDK_INT >= android.os.Build.VERSION_CODES. O ) {
+                    AudioAttributes audioAttributes = new AudioAttributes.Builder()
+                            .setContentType(AudioAttributes. CONTENT_TYPE_SONIFICATION )
+                            .setUsage(AudioAttributes. USAGE_ALARM )
+                            .build() ;
+                    int importance = NotificationManager. IMPORTANCE_HIGH ;
+                    NotificationChannel notificationChannel = new
+                            NotificationChannel( NOTIFICATION_CHANNEL_ID , "NOTIFICATION_CHANNEL_NAME" , importance) ;
+                    notificationChannel.setLightColor(Color. RED ) ;
+                    notificationChannel.enableVibration( true ) ;
+                    mBuilder.setProgress(100, per, false);
+                    mBuilder.setChannelId( NOTIFICATION_CHANNEL_ID ) ;
+                    assert mNotificationManager != null;
+                    mNotificationManager.createNotificationChannel(notificationChannel) ;
+                }
+                // Displays the progress bar for the first time.
+                notificationManager.notify(1,mBuilder.build());
+            }
+            counter++;
+
+        }
+
+    }
+
+    private void requestPermissionAndContinue() {
+        if (ContextCompat.checkSelfPermission(this, WRITE_EXTERNAL_STORAGE) != PackageManager.PERMISSION_GRANTED
+                && ContextCompat.checkSelfPermission(this, READ_EXTERNAL_STORAGE) != PackageManager.PERMISSION_GRANTED) {
+
+            if (ActivityCompat.shouldShowRequestPermissionRationale(this, WRITE_EXTERNAL_STORAGE)
+                    && ActivityCompat.shouldShowRequestPermissionRationale(this, READ_EXTERNAL_STORAGE)) {
+                AlertDialog.Builder alertBuilder = new AlertDialog.Builder(this);
+                alertBuilder.setCancelable(true);
+                alertBuilder.setTitle("permission_necessary");
+                alertBuilder.setMessage("storage_permission_is_encessary_to_wrote_event");
+                alertBuilder.setPositiveButton(android.R.string.yes, new DialogInterface.OnClickListener() {
+                    @TargetApi(Build.VERSION_CODES.JELLY_BEAN)
+                    public void onClick(DialogInterface dialog, int which) {
+                        ActivityCompat.requestPermissions(MainActivity.this, new String[]{WRITE_EXTERNAL_STORAGE
+                                , READ_EXTERNAL_STORAGE}, 1);
+                    }
+                });
+                AlertDialog alert = alertBuilder.create();
+                alert.show();
+                Log.e("", "permission denied, show dialog");
+            } else {
+                ActivityCompat.requestPermissions(MainActivity.this, new String[]{WRITE_EXTERNAL_STORAGE,
+                        READ_EXTERNAL_STORAGE}, 1);
+            }
+        } else {
+            new DownloadFiles().execute();
+        }
+    }
+
+    private boolean checkPermission() {
+
+        return ContextCompat.checkSelfPermission(this, WRITE_EXTERNAL_STORAGE) != PackageManager.PERMISSION_GRANTED
+                && ContextCompat.checkSelfPermission(this, READ_EXTERNAL_STORAGE) != PackageManager.PERMISSION_GRANTED;
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+
+        if (requestCode == 1) {
+            if (permissions.length > 0 && grantResults.length > 0) {
+
+                boolean flag = true;
+                for (int i = 0; i < grantResults.length; i++) {
+                    if (grantResults[i] != PackageManager.PERMISSION_GRANTED) {
+                        flag = false;
+                    }
+                }
+                if (flag) {
+                    new DownloadFiles().execute();
+                } else {
+                    finish();
+                }
+
+            } else {
+                finish();
+            }
+        } else {
+            super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+        }
+    }
+}
